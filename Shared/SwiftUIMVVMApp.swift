39c52a994e9d01ef2eb9b3d0005ddff958cb6ab7
Ronald Mannak <ronaldmannak@me.com>
2022-01-18 12:34:14 -0800

    Initial Commit

---
+++ Shared/SwiftUIMVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMVVMApp.swift
+//  Shared
+//
+//  Created by Ronald Mannak on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
